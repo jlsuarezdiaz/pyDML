0b88a2cf60020090b618b574f53712038b0fc961
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2018-02-04 19:37:42 +0100

    Added knn plotting

--- dml/knn_plot.py
+++ dml/knn_plot.py
@@ -13,20 +13,18 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 from sklearn.preprocessing import LabelEncoder
-
 from sklearn.neighbors import KNeighborsClassifier
 
-def knn_plot(X,y,k,knn_clf = None, fitted = False, title = None, subtitle=None, xrange = None, yrange = None, 
-             xlabel = None, ylabel = None, grid_step = [0.1,0.1],
-             label_legend = True, legend_loc="lower right", cmap=None, plot_points = True, plot_regions = True,
-             region_intensity = 0.4,legend_plot_points=True, legend_plot_regions=True,**fig_kw):
-    if knn_clf is None:
-        fitted = False
-        knn_clf = KNeighborsClassifier(n_neighbors=k)
-        
+from .dml_utils import metric_to_linear
+
+def classifier_plot(X,y,clf, fitted=False, f=None, ax=None, title = None, subtitle=None, xrange=None, yrange=None,
+                    xlabel=None, ylabel=None, grid_step=[0.1,0.1], label_legend=True, legend_loc="lower right",
+                    cmap=None, label_colors=None, plot_points=True, plot_regions=True,
+                    region_intensity=0.4,legend_plot_points=True, legend_plot_regions=True,**fig_kw):
+    
     if not fitted:
-        knn_clf.fit(X,y)
-        
+        clf.fit(X,y)
+    
     # Plot boundaries
     if xrange is None:
         x_min, x_max = X[:,0].min() - 1, X[:,0].max() + 1
@@ -38,27 +36,37 @@ def knn_plot(X,y,k,knn_clf = None, fitted = False, title = None, subtitle=None, 
     else:
         y_min, y_max = yrange[0], yrange[1]
         
-    #le = LabelEncoder()
-    #yn = le.fit_transform(y)
-    
     # Grid
     xx, yy = np.meshgrid(np.arange(x_min,x_max,grid_step[0]), np.arange(y_min, y_max,grid_step[1]))
     
     # Plot
-    f, ax = plt.subplots(sharex='col',sharey='row',**fig_kw)
-    
-    # Grid predictions
-    Z = knn_clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
+    if f is None or ax is None:
+        f, ax = plt.subplots(sharex='col',sharey='row',**fig_kw)   
     
     cls = np.unique(y)
+    le = LabelEncoder()
+    ncls = le.fit_transform(cls)
+    
+    # Grid predictions
+    Z = le.transform(clf.predict(np.c_[xx.ravel(), yy.ravel()]))
+    Z = Z.reshape(xx.shape)
+    
+    if label_colors is not None:
+        color_list = np.array(label_colors)[le.transform(y)]
+    
     
     if plot_regions: # Region plot
-        cont = ax.contourf(xx,yy,Z,10*len(cls),alpha=region_intensity,cmap=cmap)
+        if label_colors is None:
+            cont = ax.contourf(xx,yy,Z,20*len(ncls),alpha=region_intensity,cmap=cmap)
+        else:
+            cont = ax.contourf(xx,yy,Z,colors=label_colors,alpha=region_intensity,levels=[j-0.5 for j in ncls]+[max(ncls)+0.5])
         
     if plot_points: # Scatter plot
-        sc = ax.scatter(X[:,0],X[:,1],c=y,s=20,edgecolor='k',cmap=cmap)
-        
+        if label_colors is None:
+            sc = ax.scatter(X[:,0],X[:,1],c=le.transform(y),s=20,edgecolor='k',cmap=cmap)
+        else:
+            sc = ax.scatter(X[:,0],X[:,1],c=color_list,s=20,edgecolor='k')
+            
     if title is not None:
         f.suptitle(title)
         
@@ -72,14 +80,94 @@ def knn_plot(X,y,k,knn_clf = None, fitted = False, title = None, subtitle=None, 
         ax.set_ylabel(ylabel)
         
     if label_legend:
-        if not legend_plot_regions:
-            handles = [plt.plot([],color=sc.get_cmap()(sc.norm(c)),ls="",marker="o", mec="k")[0] for c in cls]
-        elif not legend_plot_points:
-            handles = [mpatches.Patch(color=cont.get_cmap()(cont.norm(c)),alpha=region_intensity) for c in cls]
-        else:
-            handles = [(plt.plot([],color=sc.get_cmap()(sc.norm(c)),ls="",marker="o", mec="k")[0],
-                    mpatches.Patch(color=sc.get_cmap()(sc.norm(c)),alpha=region_intensity)) for c in cls] # ms = 5
+        if label_colors is None:    # Plot legend with color map.             
+            if not legend_plot_regions or not plot_regions: # Only plot points
+                handles = [plt.plot([],color=sc.get_cmap()(sc.norm(c)),ls="",marker="o", mec="k")[0] for c in ncls]
+            elif not legend_plot_points or not plot_points: # Only plot regions
+                handles = [mpatches.Patch(color=cont.get_cmap()(cont.norm(c)),alpha=region_intensity) for c in ncls]
+            else: # Plot all
+                handles = [(plt.plot([],color=sc.get_cmap()(sc.norm(c)),ls="",marker="o", mec="k")[0],
+                        mpatches.Patch(color=sc.get_cmap()(sc.norm(c)),alpha=region_intensity)) for c in ncls] # ms = 5
+        else:   # Plot legend with given colors
+            if not legend_plot_regions or not plot_regions: # Only plot points
+                handles = [plt.plot([],color=c,ls="",marker="o", mec="k")[0] for c in label_colors]
+            elif not legend_plot_points or not plot_points: # Only plot regions
+                handles = [mpatches.Patch(color=c,alpha=region_intensity) for c in label_colors]
+            else: # PLot all
+                handles = [(plt.plot([],color=c,ls="",marker="o", mec="k")[0],
+                        mpatches.Patch(color=c,alpha=region_intensity)) for c in label_colors] # ms = 5
+        
         ax.legend(handles,cls,loc=legend_loc)
         
-    plt.show()
+    return f
+
+def knn_plot(X,y,k=1,knn_clf = None, fitted = False, metric=None, transformer=None, dml=None, dml_fitted=False,
+             f = None, ax = None, title = None, subtitle=None, xrange = None, yrange = None, 
+             xlabel = None, ylabel = None, grid_step = [0.1,0.1], label_legend = True, legend_loc="lower right",
+             cmap=None, label_colors=None, plot_points = True, plot_regions = True,
+             region_intensity = 0.4,legend_plot_points=True, legend_plot_regions=True,**fig_kw):
+    # Fitting distance metrics
+    if dml is not None:
+        if dml_fitted:
+            X = dml.transform(X)
+        else:
+            X = dml.fit_transform(X,y)
+    elif transformer is not None:
+        X = X.dot(transformer.T)
+    elif metric is not None:
+        transformer = metric_to_linear(metric)
+        X = X.dot(transformer.T)
+    
+    
+    # Fiting classifier
+    if knn_clf is None:
+        fitted = False
+        knn_clf = KNeighborsClassifier(n_neighbors=k)
+        
+    if not fitted:
+        knn_clf.fit(X,y)
+        
+    
+    if f is None or ax is None:  
+        f, ax = plt.subplots(sharex='col',sharey='row',**fig_kw)
+    
+    return classifier_plot(X,y,knn_clf,True,f,ax,title,subtitle,xrange,yrange,xlabel,ylabel,
+                           grid_step,label_legend,legend_loc,cmap,label_colors,plot_points,
+                           plot_regions,region_intensity,legend_plot_points,legend_plot_regions)
+        
+def knn_multiplot(X,y,nrow=None,ncol=None,ks=None, clfs=None, fitted = False, metrics=None, transformers=None, dmls=None, dml_fitted=False,
+                  title = None, subtitles = None, xlabels=None, ylabels=None, grid_step=[0.1,0.1,],
+                  label_legend=True, legend_loc="lower right",cmap=None, label_colors=None,plot_points=True,plot_regions=True, region_intensity=0.4,
+                  legend_plot_points=True, legend_plot_regions=True,**fig_kw):
+    
+    # Look for not None parameter between ks, knn_clfs, metrics, transformers and dmls
+    param_list = ks if ks is not None else clfs if clfs is not None else metrics if metrics is not None else transformers if transformers is not None else dmls if dmls is not None else None
+    
+    # Axes array plot
+    if nrow is None or ncol is None:
+        ncol=2
+        nrow = (len(param_list)+1)/2
+        
+    f, axarr = plt.subplots(nrow,ncol,**fig_kw)
+    
+    if param_list is not None:
+        for i in range(len(param_list)):
+            k = ks[i] if ks is not None else None
+            clf = clfs[i] if clfs is not None else None
+            metric = metrics[i] if metrics is not None else None
+            transformer = transformers[i] if transformers is not None else None
+            dml = dmls[i] if dmls is not None else None
+            subtitle = subtitles[i] if subtitles is not None else None
+            
+            ix0, ix1 = i // ncol, i % ncol
+            ax = axarr[ix0,ix1]
+            knn_plot(X,y,k,clf,fitted,metric,transformer,dml,dml_fitted,f,ax,title,subtitle,None,None,None,None,grid_step,label_legend,legend_loc,
+                     cmap,label_colors,plot_points,plot_regions,region_intensity,legend_plot_points,legend_plot_regions)
+    
+    return f
+        
+    
+    
+    
+    
     
--- test/testplot.py
+++ test/testplot.py
@@ -10,9 +10,10 @@ from utils import(
         read_ARFF, kfold_multitester_supervised_knn, toy_datasets)
 
 from dml import(
-    NCA,LDA,RCA,PCA,LMNN,ANMM,LSI,kNN, knn_plot)
+    NCA,LDA,RCA,PCA,LMNN,ANMM,LSI,kNN, knn_plot, classifier_plot, knn_multiplot)
 
 from sklearn.datasets import load_digits
+from sklearn import svm
 
 import numpy as np
 
@@ -27,34 +28,38 @@ def digits_data():
     return X,y
 
 #X,y = toy_datasets.digits_toy_dataset(dims=[0,1],numbers=[7,8])
-#X,y = toy_datasets.circular_toy_dataset(rads=[1,2,3],samples=[200,200,200],noise=[0.4,0.4,0.4],seed=seed)
-#X,y = toy_datasets.hiperplane_toy_dataset(seed=seed)
-#X,y = toy_datasets.iris2d_toy_dataset()
-X,y = toy_datasets.balls_toy_dataset(seed=seed)
-#X,y = toy_datasets.single_toy_dataset(seed=seed)
-
-#X = np.array([[3,1],[4,2],[3,4],[5,4],[-1,1],[1,2],[2,2],[3,3]])
-#y = np.array([1,1,1,1,-1,-1,-1,-1])
-#y = np.array(['OK','OK','OK','OK','ERR','ERR','ERR','ERR'])
-
-#X = np.array([[0,0],[0,1],[1.1,0],[2,0]])
-#y = np.array([1,1,-1,-1])
     
-#X, y = digits_data()
-#selected = np.where(np.isin(y,[0,1,3,4,6,9]))[0]
-#X, y = X[selected,:], y[selected]
-knn_plot(X,y,k=3,cmap="gist_rainbow",figsize=(10,8))
 
-nca = NCA(max_iter=10000, learning_rate = 0.1, initial_transform = "scale", descent_method = "BGD")
+X,y = toy_datasets.circular_toy_dataset(rads=[1,2,3],samples=[200,200,200],noise=[0.4,0.4,0.4],seed=seed)
+knn_plot(X,y,k=3,cmap="gist_rainbow",figsize=(15,8))
+
+
+X,y = toy_datasets.hiperplane_toy_dataset(seed=seed)
+knn_plot(X,y,k=3,cmap="gist_rainbow",figsize=(15,8))
+
+
+X,y = toy_datasets.iris2d_toy_dataset()
+iris_labels = ['Setosa' if yi == 0 else 'Versicolor' if yi == 1 else 'Virginica' for yi in y]
+knn_plot(X,iris_labels,region_intensity=0.2,k=3,label_colors=["red", "green", "blue"],figsize=(15,8))
+
+
+X,y = toy_datasets.balls_toy_dataset(seed=seed)
+knn_plot(X,y,k=3,label_colors=['red','blue','green','orange','purple'],figsize=(15,8))
+
+X = np.array([[3,1],[4,2],[3,4],[5,4],[-1,1],[1,2],[2,2],[3,3]])
+y = np.array(['OK','OK','OK','OK','ERR','ERR','ERR','ERR'])
+knn_plot(X,y,k=3,label_colors=["red","blue"],figsize=(15,8))
+
+X = np.array([[0,0],[0,1],[1.1,0],[2,0]])
+y = np.array(['RED','RED','BLUE','BLUE'])
+knn_plot(X,y,k=3,label_colors=["red","blue"],figsize=(15,8))
+    
+X, y = digits_data()
+selected = np.where(np.isin(y,[0,1,3,4,6,9]))[0]
+X, y = X[selected,:], y[selected]
 lda = LDA(num_dims = 2)
-#rca = RCA()
-pca = PCA(thres = 0.95)
-lmnn = LMNN(max_iter=100000,learning_rate = "adaptive",eta0=0.001, k = 1, mu = 0.5,tol=0,prec=0)
 anmm = ANMM(num_dims = 2, n_friends = 1,n_enemies = 1)
-lsi = LSI(supervised=True, err = 1e-10, itproj_err = 1e-10)
-
 alg = lda
-
 alg.fit(X,y)
 Xn = alg.transform(X)
 if(Xn.shape[1] < 2):
@@ -62,5 +67,20 @@ if(Xn.shape[1] < 2):
     for i in range(Xn.shape[0]):
         X2[i,:] = [Xn[i,0],0.0]
     Xn = X2
+knn_plot(Xn,y,k=3,cmap="gist_rainbow",region_intensity=0.4,legend_plot_points=True,figsize=(15,8))
 
-#knn_plot(Xn,y,k=3,cmap="gist_rainbow",region_intensity=0.4,legend_plot_points=True,figsize=(15,8))
+svmc = svm.SVC()
+X = np.array([[3,1],[4,2],[3,4],[5,4],[-1,1],[1,2],[2,2],[3,3]])
+y = np.array(['OK','OK','OK','OK','ERR','ERR','ERR','ERR'])
+classifier_plot(X,y,svmc,label_colors=['red','blue'],figsize=(15,8))
+
+X, y = digits_data()
+selected = np.where(np.isin(y,[0,1,3,4,6,9]))[0]
+#X, y = X[selected,:], y[selected]
+lda = LDA(num_dims = 2)
+knn_plot(X,y,k=3,dml=lda,cmap="gist_rainbow",figsize=(15,8))
+
+anmm = ANMM(num_dims = 2, n_friends = 1,n_enemies = 1)
+f = knn_multiplot(X,y,nrow=2,ncol=2,ks=[1,1,11,11],dmls=[lda,anmm,lda,anmm],title="Comparación de DMLS",subtitles=["k=1, LDA", "k=1, ANMM", "k=11, LDA", "k=11, ANMM"],
+              cmap="gist_rainbow",plot_points=True,figsize=(20,16))
+
